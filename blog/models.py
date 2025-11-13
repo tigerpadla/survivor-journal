c442d4f7b36fef8d2784ed264193594d04116a27
tigerpadla <xythryll@gmail.com>
2025-11-13 15:00:04 +0000

    Add comment model

--- blog/models.py
+++ blog/models.py
@@ -17,3 +17,11 @@ class Post(models.Model):
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(max_length=300, blank=True)
     updated_on = models.DateTimeField(auto_now=True)
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="commenter")
+    body = models.TextField(max_length=500)
+    approved = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
