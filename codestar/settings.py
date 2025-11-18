1f74cf4e1780ac6886673f4ecdf01035b3cb1aa1
tigerpadla <xythryll@gmail.com>
2025-11-18 13:42:23 +0000

    Add about app

--- codestar/settings.py
+++ codestar/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "blog",
+    "about",
     "django_summernote",
 ]
 
@@ -132,7 +133,7 @@ STATIC_URL = "static/"
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
