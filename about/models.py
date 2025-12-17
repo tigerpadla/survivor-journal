924ca20561ef989c79cace23e5806e563f4b9fa6
tigerpadla <xythryll@gmail.com>
2025-12-17 11:41:15 +0000

    Import CodeStar template and setup enviroment variables

--- about/models.py
+++ about/models.py
@@ -15,16 +15,3 @@ class About(models.Model):
 
     def __str__(self):
         return self.title
-
-
-class CollaborateRequest(models.Model):
-    """
-    Stores a single collaboration request message
-    """
-    name = models.CharField(max_length=200)
-    email = models.EmailField()
-    message = models.TextField()
-    read = models.BooleanField(default=False)
-
-    def __str__(self):
-        return f"Collaboration request from {self.name}"
