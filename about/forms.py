01b424a25508017e67cb5bb5b7158cbe0c040909
ColeiosCode <jdburkecole@gmail.com>
2025-12-17 15:53:01 +0000

    changed footer,removed collaborate section

--- about/forms.py
+++ about/forms.py
@@ -1,12 +1,3 @@
 from django import forms
 
 
-class CollaborateForm(forms.Form):
-    """
-    Simple form for collaboration requests. This is a plain
-    Form (not a ModelForm) because the CollaborateRequest model
-    was removed.
-    """
-    name = forms.CharField(max_length=200)
-    email = forms.EmailField()
-    message = forms.CharField(widget=forms.Textarea)
