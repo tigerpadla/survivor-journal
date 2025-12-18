bc9416cfce891dc11942e8654ab4326f23a098d7
davidjmolina55-lgtm <d.molina55@hotmail.com>
2025-12-18 13:07:39 +0000

    add image

--- codestar/settings.py
+++ codestar/settings.py
@@ -155,7 +155,7 @@ MESSAGE_TAGS = {
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
